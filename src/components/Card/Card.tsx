659f0128d825ca378bebc424928fe86c4b5c9332
HankTech <hanktech1.0@gmail.com>
2023-02-08 23:01:32 -0400

    card

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -6,7 +6,7 @@ import {ArticlesTypes} from '../../types'
 
 export default function Card({image, title, content}: ArticlesTypes) {
   return (
-    <div className='max-w-[300px]'>
+    <div className='group max-w-[300px] mb-16 shadow-lg pb-10 rounded-[10px]'>
       <Image
         className='w-full rounded-t-[10px]'
         src={image}
@@ -15,8 +15,9 @@ export default function Card({image, title, content}: ArticlesTypes) {
         height={200}
       />
       <div className='px-5'>
-        <h3 className='font-OpenSans font-bold text-lg text-center text-[#3F454A] my-2'>{title}</h3>
+        <h3 className='font-OpenSans font-bold text-lg text-center text-[#3F454A] my-5 group-hover:text-[#D8AD3D]'>{title}</h3>
         <p className='font-OpenSans font-normal text-base text-center text-[#3F454A]'>{content}</p>
+        <p className='group-hover:visible invisible uppercase font-OpenSans font-bold text-base pl-5 mt-6 text-[#D8AD3D]'>Ver mas</p>
       </div>
     </div>
   )
